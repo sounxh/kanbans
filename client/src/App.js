a87d465403b37785e52df51d43175c9635725dc0
anas zabili <anaszab78@gmail.com>
2021-01-16 17:02:37 +0100

    commit route application front

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
-import SelectBoard from './components/SelectBoard'
+import SelectBoard from "./components/SelectBoard";
+import Home from "./components/Home";
+import Login from './components/Login'
+import { Route, BrowserRouter, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       {/* penser a bien lui passer le user conecter quand cette partie sera faite */}
       {/* <SelectBoard/> */}
-      <Header />
-      <Body />
-    </>
+      {/* <Header />
+      <Body /> */}
+      <Switch>
+        <Route path="/" exact component={SelectBoard} />
+        <Route path="/" exact component={SelectBoard} />
+        <Route path="/Home" component={Home} />
+        <Route path="/Login" component={Login} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
